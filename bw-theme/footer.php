b49a88551914ccf49700f68b71cdeae2c6271b74
Catherine <60444425+MonkeyKingBar-bit@users.noreply.github.com>
2022-12-20 10:11:31 +0300

    add: folder with data for wp

---
+++ bw-theme/footer.php
@@ -0,0 +1,12 @@
+   <footer class="footer">
+      <div class="container">
+         <div class="footer__text">
+            Copyright &copy; Kenan Hamidic. All rights reserved.
+         </div>
+      </div>
+   </footer>
+
+   <?php wp_footer(); ?>
+</body>
+
+</html>
